16b2dc932399cd4c3668bd913f71b13176891c19
Abdoul <abdoulniyigena@gmail.com>
2020-03-30 21:32:02 +0200

    use react context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import SearchParams from "./searchParams";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("darkblue");
   return (
-    <div>
-      <header>
-        <Link to="/">
-          <h1>Adopt Me!</h1>
-        </Link>
-      </header>
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
   );
 };
 
