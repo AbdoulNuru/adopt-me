1ad2855397f47de75ea9cda039820cf90cf4eeda
Abdoul <abdoulniyigena@gmail.com>
2020-03-30 20:48:03 +0200

    use error boundary

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-console */
+import React, { Component } from "react";
+import { Link, Redirect } from "@reach/router";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false, redirect: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error <Link to="/">Click here </Link> to back to the
+          homepage
+        </h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
